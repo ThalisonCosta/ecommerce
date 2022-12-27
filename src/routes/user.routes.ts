265a8512f0b0b4c22e41b2aed50880247ad0fe99
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-27 00:36:01 -0400

    :passport_control: user middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { CreateUserController } from '../modules/users/useCases/createUser/CreateUserController';
+import { CreateUserMiddleware } from '../modules/users/useCases/createUser/CreateUserMiddleware';
 
 const userRoutes = Router();
 const createUserController = new CreateUserController();
+const createUserMiddleware = new CreateUserMiddleware();
 
-userRoutes.post('/', createUserController.handle);
+userRoutes.post('/', createUserMiddleware.validate , createUserController.handle);
 
 export { userRoutes };
