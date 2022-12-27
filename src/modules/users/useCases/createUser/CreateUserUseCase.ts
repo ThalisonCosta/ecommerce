832875da33aa1b4d7303df16ec05946b86738271
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-27 00:34:32 -0400

    :art: separated middleware

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,19 +1,14 @@
 import { Users } from '@prisma/client';
-import { AppError } from '../../../../errors/AppError';
 import { prisma } from '../../../../prisma/client';
 import { CreateUserDTO } from '../../dtos/CreateUserDTO';
 
 export class CreateUserUseCase {
   async execute({name, email, password}:CreateUserDTO): Promise<Users> {
-    const userExists = await prisma.users.findUnique({
+    await prisma.users.findUnique({
       where: {
         email: email
       }
     });
-
-    if(userExists){
-      throw new AppError('user already created!');
-    }
 
     const user = await prisma.users.create({
       data:{
