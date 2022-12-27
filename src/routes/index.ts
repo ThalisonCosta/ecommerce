28b1d45da340083ccd0e5bda499daf643899627c
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-27 00:35:29 -0400

    :construction: post a product route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { productRoute } from './product.routes';
 import { userRoutes } from './user.routes';
 
 const routes = Router();
 
 routes.use('/user', userRoutes);
+routes.use('/products', productRoute);
 
 export { routes };
