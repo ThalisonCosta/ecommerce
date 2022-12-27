8fadb9e8f72f5da037a2f7a89a4b05cbe7345033
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-27 00:31:34 -0400

    :label:

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'express-async-errors';
-import express, {NextFunction, Request, response, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import { routes } from './routes';
 import { AppError } from './errors/AppError';
 
@@ -7,13 +7,13 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use((err: Error, req: Request, res: Response, next: NextFunction )=>{
+app.use((err: Error, req: Request, res: Response, _next: NextFunction )=>{
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       message: err.message
     });
   }
-  return response.status(500).json({
+  return res.status(500).json({
     message: `Internal Server Error ${err.message}`
   });
 });
