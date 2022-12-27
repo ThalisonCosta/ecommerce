77edf0224137b12601e01381340894570bbb0963
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-26 22:26:29 -0400

    :construction: initiate server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
-import { App } from './app';
+import 'express-async-errors';
+import express, {NextFunction, Request, response, Response} from 'express';
+import { routes } from './routes';
+import { AppError } from './errors/AppErro';
 
-new App().server.listen(8080);
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use((err: Error, req: Request, res: Response, next: NextFunction )=>{
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message
+    });
+  }
+  return response.status(500).json({
+    message: `Internal Server Error ${err.message}`
+  });
+});
+
+app.listen(8080, ()=> console.log('server is running on port 8080'));
