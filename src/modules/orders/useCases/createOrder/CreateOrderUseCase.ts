d31b9ad4413c66ab4c76515f9e2850b2757ec035
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-29 21:29:22 -0400

    :children_crossing: more details from the product

--- src/modules/orders/useCases/createOrder/CreateOrderUseCase.ts
+++ src/modules/orders/useCases/createOrder/CreateOrderUseCase.ts
@@ -9,6 +9,20 @@ export class CreateOrderUseCase {
         productId,
         quantity,
         userId,
+      },
+      include:{
+        product:{
+          select:{
+            category: true,
+            id: true,
+            name:true,
+            price:true,
+            image: true,
+            description: true,
+            userOrder: false,
+            created_at:true
+          }
+        }
       }
     });
     return order;
