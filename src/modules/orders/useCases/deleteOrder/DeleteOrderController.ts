74c501285761a3c7a2fb71e39bb5ff5fca9fa28a
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-30 19:59:52 -0400

    :passport_control: jwt to delete order

--- src/modules/orders/useCases/deleteOrder/DeleteOrderController.ts
+++ src/modules/orders/useCases/deleteOrder/DeleteOrderController.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
+import { decode } from '../../../../utils/commom';
 import { DeleteOrderUseCase } from './DeleteOrderUseCase';
 
+const deleteOrderUseCase = new DeleteOrderUseCase();
 export class DeleteOrderController{
   async handle(req:Request, res:Response){
     const { id } = req.params;
-    const deleteOrderUseCase = new DeleteOrderUseCase();
-    deleteOrderUseCase.execute(Number(id));
-    res.status(204).send();
+    const { authorization } = req.headers;
+    if(authorization){
+      let userId = decode(authorization);
+      userId = String(userId);
+      deleteOrderUseCase.execute(Number(id), userId);
+      res.status(204).send();
+    }
   }
 }
--- src/modules/orders/useCases/deleteOrder/DeleteOrderUseCase.ts
+++ src/modules/orders/useCases/deleteOrder/DeleteOrderUseCase.ts
@@ -1,10 +1,11 @@
 import { prisma } from '../../../../prisma/client';
 
 export class DeleteOrderUseCase{
-  async execute(id:number):Promise<void>{
-    await prisma.orders.delete({
+  async execute(id:number, userId:string):Promise<void>{
+    await prisma.orders.deleteMany({
       where:{
-        id
+        id,
+        userId
       }
     });
   }
