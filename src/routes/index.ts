f47def33b437156963318494aa0b5e13de9a3897
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-29 22:20:24 -0400

    :construction: category route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { productRoute } from './product.routes';
 import { userRoutes } from './user.routes';
 import { orderRoute } from './order.routes';
+import { categoryRoute } from './category.routes';
 const routes = Router();
 
 routes.use('/user', userRoutes);
 routes.use('/products', productRoute);
 routes.use('/orders', orderRoute);
+routes.use('/categories', categoryRoute);
 
 export { routes };
