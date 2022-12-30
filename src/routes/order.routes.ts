cc41d07b855d74b3c38a3ac264861a3246be9287
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-29 21:38:46 -0400

    :construction: delete orders route

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CreateOrderController } from '../modules/orders/useCases/createOrder/CreateOrderController';
+import { DeleteOrderController } from '../modules/orders/useCases/deleteOrder/DeleteOrderController';
 import { ListOrderController } from '../modules/orders/useCases/listOrder/ListOrderController';
 import { ListOrderByIdController } from '../modules/orders/useCases/listOrderById/ListOrderByIdController';
 
@@ -7,9 +8,11 @@ const orderRoute = Router();
 const createOrderController = new CreateOrderController();
 const listOrderController = new ListOrderController();
 const listOrderByIdController = new ListOrderByIdController();
+const deleteOrderController = new DeleteOrderController();
 
 orderRoute.post('/', createOrderController.handle); 
 orderRoute.get('/', listOrderController.handle);
 orderRoute.get('/:id', listOrderByIdController.handle);
+orderRoute.delete('/:id', deleteOrderController.handle);
 
 export { orderRoute };
