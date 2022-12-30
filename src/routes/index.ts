b63789e599b4334a5680cb2404b1b6e5a8cfce59
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-29 20:57:38 -0400

    :construction: create orders route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { productRoute } from './product.routes';
 import { userRoutes } from './user.routes';
-
+import { orderRoute } from './order.routes';
 const routes = Router();
 
 routes.use('/user', userRoutes);
 routes.use('/products', productRoute);
+routes.use('/orders', orderRoute);
 
 export { routes };
