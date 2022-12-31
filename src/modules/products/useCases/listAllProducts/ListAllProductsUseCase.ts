49e3a7d30d511b0e3b2493d48cb6c0c08f94792e
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-31 17:38:37 -0400

    :construction: select items to show

--- src/modules/products/useCases/listAllProducts/ListAllProductsUseCase.ts
+++ src/modules/products/useCases/listAllProducts/ListAllProductsUseCase.ts
@@ -1,12 +1,29 @@
 import { Products } from '@prisma/client';
 import { prisma } from '../../../../prisma/client';
 
+type noUserId = Omit<Products, 'userId'>
+
 export class ListAllProductsUseCase{
-  async execute(): Promise<Products[]>{
+  async execute():Promise<noUserId[]>{
     const products = await prisma.products.findMany({
       orderBy: {
         created_at: 'desc'
       },
+      select:{
+        userId:false,
+        id:true,
+        name:true,
+        price:true,
+        image:true,
+        description:true,
+        created_at:true,
+        categoryId:true,
+        user:{
+          select:{
+            name:true
+          }
+        }
+      }
     });
     return products;
   }
