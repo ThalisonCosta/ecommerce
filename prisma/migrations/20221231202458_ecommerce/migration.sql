71b7f86f59ec9953417b10f0a384eab4990f2a75
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-31 17:40:58 -0400

    :building_construction:

--- prisma/migrations/20221231202458_ecommerce/migration.sql
+++ prisma/migrations/20221231202458_ecommerce/migration.sql
@@ -9,6 +9,7 @@ CREATE TABLE `categories` (
 -- CreateTable
 CREATE TABLE `products` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(191) NOT NULL,
     `categoryId` INTEGER NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `price` DECIMAL(65, 30) NOT NULL,
@@ -40,6 +41,9 @@ CREATE TABLE `orders` (
     UNIQUE INDEX `orders_id_key`(`id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `products` ADD CONSTRAINT `products_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `products` ADD CONSTRAINT `products_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
