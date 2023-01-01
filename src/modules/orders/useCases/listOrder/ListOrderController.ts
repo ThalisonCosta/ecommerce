f4299684d5ce7d3069457ba6b81c18dff6c8e68a
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-01 13:42:36 -0400

    :art:

--- src/modules/orders/useCases/listOrder/ListOrderController.ts
+++ src/modules/orders/useCases/listOrder/ListOrderController.ts
@@ -1,17 +1,14 @@
 import { Request, Response } from 'express';
 import { decode } from '../../../../utils/commom';
-import { ListOrderUseCase } from './ListOrderUseCase';
+import { listOrderUseCase } from './ListOrderUseCase';
 
-const listOrderController = new ListOrderUseCase();
 
-export class ListOrderController {
-  async handle(req:Request, res: Response){
-    const { authorization } = req.headers;
-    if(authorization){
-      const id = decode(authorization);
-      const userId = String(id);
-      const orders = await listOrderController.execute(userId);
-      return res.status(200).json(orders);
-    }
+export const listOrderController = async (req:Request, res: Response) => {
+  const { authorization } = req.headers;
+  if(authorization){
+    const id = decode(authorization);
+    const userId = String(id);
+    const orders = await listOrderUseCase(userId);
+    return res.status(200).json(orders);
   }
-}
+};
--- src/modules/orders/useCases/listOrder/ListOrderUseCase.ts
+++ src/modules/orders/useCases/listOrder/ListOrderUseCase.ts
@@ -1,16 +1,14 @@
 import { Orders } from '@prisma/client';
 import { prisma } from '../../../../prisma/client';
 
-export class ListOrderUseCase {
-  async execute(userId:string): Promise<Orders[]>{
-    const orders = await prisma.orders.findMany({
-      where:{
-        userId
-      },
-      include:{
-        product:true
-      }
-    });
-    return orders;
-  }
-}
+export const listOrderUseCase = async (userId:string): Promise<Orders[]> => {
+  const orders = await prisma.orders.findMany({
+    where:{
+      userId
+    },
+    include:{
+      product:true
+    }
+  });
+  return orders;
+};
