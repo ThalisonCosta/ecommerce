5f9381ad7bc3f7a8b4c0be88830e18dd3e671e1c
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-01 13:42:24 -0400

    :art:

--- src/modules/orders/useCases/createOrder/CreateOrderController.ts
+++ src/modules/orders/useCases/createOrder/CreateOrderController.ts
@@ -1,20 +1,16 @@
 import { Request, Response } from 'express';
 import { decode } from '../../../../utils/commom';
-import { CreateOrderUseCase } from './CreateOrderUseCase';
+import { createOrderUseCase } from './CreateOrderUseCase';
 
-const createOrderUseCase = new CreateOrderUseCase();
 
-export class CreateOrderController{
-  async handle(req:Request, res:Response){
-    const { productId, quantity } = req.body;
-    const { authorization } = req.headers;
+export const createOrderController = async (req:Request, res:Response) =>{
+  const { productId, quantity } = req.body;
+  const { authorization } = req.headers;
 
-    if(authorization){
-      const id = decode(authorization);
-      const userId = String(id);
-      const result = await createOrderUseCase.execute({productId, quantity, userId});
-      return res.status(201).json(result);
-    }
-
+  if(authorization){
+    const id = decode(authorization);
+    const userId = String(id);
+    const result = await createOrderUseCase({productId, quantity, userId});
+    return res.status(201).json(result);
   }
-}
+};
--- src/modules/orders/useCases/createOrder/CreateOrderUseCase.ts
+++ src/modules/orders/useCases/createOrder/CreateOrderUseCase.ts
@@ -2,30 +2,27 @@ import { Orders } from '@prisma/client';
 import { prisma } from '../../../../prisma/client';
 import { CreateOrderDTO } from '../../dtos/CreateOrderDTO';
 
-export class CreateOrderUseCase {
-  async execute({productId, quantity, userId}:CreateOrderDTO): Promise<Orders>{
-    
-    const order = await prisma.orders.create({
-      data: {
-        productId,
-        quantity,
-        userId,
-      },
-      include:{
-        product:{
-          select:{
-            category: true,
-            id: true,
-            name:true,
-            price:true,
-            image: true,
-            description: true,
-            userOrder: false,
-            created_at:true
-          }
+export const createOrderUseCase = async({productId, quantity, userId}:CreateOrderDTO): Promise<Orders> => {
+  const order = await prisma.orders.create({
+    data: {
+      productId,
+      quantity,
+      userId,
+    },
+    include:{
+      product:{
+        select:{
+          category: true,
+          id: true,
+          name:true,
+          price:true,
+          image: true,
+          description: true,
+          userOrder: false,
+          created_at:true
         }
       }
-    });
-    return order;
-  }
-}
+    }
+  });
+  return order;
+};
