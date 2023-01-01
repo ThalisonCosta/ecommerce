49ee703625b00c94c8e56bd69c1bb832e7b2f17d
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-01 16:55:48 -0400

    :children_crossing: remove userId from response

--- src/modules/products/useCases/listProductById/ListProductByIdUseCase.ts
+++ src/modules/products/useCases/listProductById/ListProductByIdUseCase.ts
@@ -4,6 +4,21 @@ export const listProductByIdUseCase = async (id:number):Promise<unknown> =>{
   const product = await prisma.products.findUnique({
     where:{
       id
+    },
+    select:{
+      category:true,
+      id:true,
+      name:true,
+      price:true,
+      image:true,
+      description:true,
+      user:{
+        select:{
+          name:true
+        }
+      },
+      userId:false,
+      created_at:true
     }
   });
     
