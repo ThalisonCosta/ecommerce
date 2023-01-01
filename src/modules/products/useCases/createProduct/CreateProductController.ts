16d011bf290373e11c4efd4f7b8b5149269e930c
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-01 18:16:18 -0400

    :passport_control: validate body

--- src/modules/products/useCases/createProduct/CreateProductController.ts
+++ src/modules/products/useCases/createProduct/CreateProductController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
+import { AppError } from '../../../../errors/AppError';
 import { decode } from '../../../../utils/commom';
 import { createProductUseCase } from './CreateProductUseCase';
 
 
 export const createProductController = async (req:Request, res:Response) => {
-  const {categoryId, name, price, description} = req.body;
-  if(req.file){
+  if(!req.body.categoryId || !req.body.name || !req.body.price || !req.body.description || !req.file){
+    throw new AppError('invalid body');
+  } else{
+    const {categoryId, name, price, description} = req.body;
     const image = req.file.filename;
     if (req.headers.authorization) {
       const userId = String(decode(req.headers.authorization));
