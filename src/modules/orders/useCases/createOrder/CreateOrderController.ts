400339b9b3c5414a0603c4b0c97aeb417e3a0d2e
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-03 10:21:54 -0400

    :goal_net: validate productId

--- src/modules/orders/useCases/createOrder/CreateOrderController.ts
+++ src/modules/orders/useCases/createOrder/CreateOrderController.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from 'express';
+import { AppError } from '../../../../errors/AppError';
 import { decode } from '../../../../utils/commom';
 import { createOrderUseCase } from './CreateOrderUseCase';
 
 
 export const createOrderController = async (req:Request, res:Response) =>{
-  const { productId, quantity } = req.body;
-  const { authorization } = req.headers;
-
-  if(authorization){
-    const id = decode(authorization);
-    const userId = String(id);
-    const result = await createOrderUseCase({productId, quantity, userId});
-    return res.status(201).json(result);
+  
+  if(!req.body.productId || !req.body.quantity){
+    throw new AppError('invalid body');
+  } else {
+    const { productId, quantity } = req.body;
+    const { authorization } = req.headers;
+    
+    if(authorization){
+      const id = decode(authorization);
+      const userId = String(id);
+      const result = await createOrderUseCase({productId, quantity, userId});
+      return res.status(201).json(result);
+    }
   }
 };
--- src/modules/orders/useCases/createOrder/CreateOrderUseCase.ts
+++ src/modules/orders/useCases/createOrder/CreateOrderUseCase.ts
@@ -1,8 +1,19 @@
 import { Orders } from '@prisma/client';
+import { AppError } from '../../../../errors/AppError';
 import { prisma } from '../../../../prisma/client';
 import { CreateOrderDTO } from '../../dtos/CreateOrderDTO';
 
 export const createOrderUseCase = async({productId, quantity, userId}:CreateOrderDTO): Promise<Orders> => {
+  const productExists = await prisma.products.findUnique({
+    where:{
+      id: productId
+    }
+  });
+  
+  if(!productExists){
+    throw new AppError('product not found', 404);
+  }
+
   const order = await prisma.orders.create({
     data: {
       productId,
