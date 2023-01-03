c54dc2256e1936974b2da3981568d82acd9e201f
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-03 10:16:34 -0400

    :goal_net:

--- src/modules/users/useCases/refreshToken/refreshTokenController.ts
+++ src/modules/users/useCases/refreshToken/refreshTokenController.ts
@@ -5,8 +5,10 @@ import { AppError } from '../../../../errors/AppError';
 export const refreshToken = async(req:Request, res:Response) => {
   if(req.body.refreshToken) {
     const {refreshToken} = req.body;
-    const token = sign({refreshToken}, process.env.JWT_KEY!, {expiresIn: '1800s'});
-    return res.status(200).json({accessToken:token});
+    if(process.env.JWT_KEY){
+      const token = sign({refreshToken}, process.env.JWT_KEY, {expiresIn: '1800s'});
+      return res.status(200).json({accessToken:token});
+    }
   }
   else {
     throw new AppError('invalid token', 401);
