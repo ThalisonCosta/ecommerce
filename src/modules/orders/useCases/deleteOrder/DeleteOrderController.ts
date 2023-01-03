5178f4af9b8e385deee66758a51281b3773aa9dc
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-03 10:42:00 -0400

    :goal_net: validate orderId

--- src/modules/orders/useCases/deleteOrder/DeleteOrderController.ts
+++ src/modules/orders/useCases/deleteOrder/DeleteOrderController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { AppError } from '../../../../errors/AppError';
 import { decode } from '../../../../utils/commom';
 import { deleteOrderUseCase } from './DeleteOrderUseCase';
 
@@ -8,7 +9,11 @@ export const deleteOrderController = async (req:Request, res:Response)=> {
   if(authorization){
     let userId = decode(authorization);
     userId = String(userId);
-    deleteOrderUseCase(Number(id), userId);
+    const orderId = Number(id);
+    if(!Number.isInteger(orderId)){
+      throw new AppError('order not found!', 404);
+    }
+    await deleteOrderUseCase(orderId, userId);
     res.status(204).send();
   }
 };
