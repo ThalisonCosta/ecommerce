09470f699984158f154c3d3c5424bde6604cca2a
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-03 10:42:21 -0400

    :goal_net: no permission to delete

--- src/modules/orders/useCases/deleteOrder/DeleteOrderUseCase.ts
+++ src/modules/orders/useCases/deleteOrder/DeleteOrderUseCase.ts
@@ -1,10 +1,26 @@
+import { AppError } from '../../../../errors/AppError';
 import { prisma } from '../../../../prisma/client';
 
 export const deleteOrderUseCase = async (id:number, userId:string):Promise<void> => {
-  await prisma.orders.deleteMany({
+  const order = await prisma.orders.findUnique({
+    where:{
+      id
+    }
+  });
+  if(!order?.id){
+    throw new AppError('order not found!', 404);
+  }
+
+  // const access = await prisma.orders.findMany({
+  // });
+  const {count} = await prisma.orders.deleteMany({
     where:{
       id,
       userId
     }
   });
+  if(count == 0){
+    throw new AppError('no access to this item!', 401);
+  }
+  
 };
