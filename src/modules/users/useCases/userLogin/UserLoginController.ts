6640d5acc431eb4238fd8a77f32991a0736ac75e
thalisonCosta <thalisoncosta123@gmail.com>
2023-01-03 10:06:30 -0400

    :art:

--- src/modules/users/useCases/userLogin/UserLoginController.ts
+++ src/modules/users/useCases/userLogin/UserLoginController.ts
@@ -1,8 +1,22 @@
+import { compare } from 'bcrypt';
 import { Request, Response } from 'express';
+import { sign } from 'jsonwebtoken';
+import { AppError } from '../../../../errors/AppError';
 import { userLoginUseCase } from './UserLoginUseCase';
 
 export const userLoginController =  async (req:Request, res:Response) => {
   const {email, password} = req.body;
-  const login = await userLoginUseCase(email, password);
-  return res.status(200).json(login);
+  const user = await userLoginUseCase(email); 
+
+  if(await compare(password, user.password)){
+    if(process.env.REFRESH_TOKEN_SECRET && process.env.JWT_KEY){
+      const refreshToken = sign({id: user.id}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '90d' });
+      const token = sign({refreshToken: refreshToken} , process.env.JWT_KEY, { expiresIn: '1800s' });
+      return res.status(200).json({
+        message: 'Authentication success!',
+        accessToken: token,
+        refreshToken: refreshToken
+      });
+    }
+  } else throw new AppError('Authentication failed', 401);
 };
--- src/modules/users/useCases/userLogin/UserLoginUseCase.ts
+++ src/modules/users/useCases/userLogin/UserLoginUseCase.ts
@@ -1,22 +1,14 @@
-import {compare} from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import { Users } from '@prisma/client';
 import { AppError } from '../../../../errors/AppError';
 import { prisma } from '../../../../prisma/client';
 
-export const userLoginUseCase = async (email:string, password:string):Promise<unknown> => {
+export const userLoginUseCase = async (email:string):Promise<Users> => {
   const user = await prisma.users.findFirst({
     where:{
       email,
     }
   });
-    
-  if (!user || !await compare(password, user.password)){
-    throw new AppError('user not found', 401);
-  } else {
-    if(await compare(password, user.password)){
-      const refreshToken = jwt.sign({id: user.id}, `${process.env.REFRESH_TOKEN_SECRET}`, {expiresIn: '90d'});
-      const accessToken = jwt.sign({ refreshToken }, `${process.env.JWT_KEY}`, {expiresIn:'1800s'});
-      return {refreshToken, accessToken};
-    }  
-  }
+  if(user){
+    return user;
+  } else throw new AppError('Authentication failed', 401);
 };
